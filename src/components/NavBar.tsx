fb4428309fe2896ce916e6b95f02065dcb282427
Jadon Hansen <jadonhansen@gmail.com>
2024-09-08 16:37:27 +0200

    added theme icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 import mapDark from "../assets/map-dark.svg";
 import tableDark from "../assets/table-dark.svg";
+import themeDark from "../assets/sun-dark.svg";
+
 import "../styles/navbar.scss";
 
 export default function NavBar() {
+	const switchTheme = () => {
+		// TODO
+		console.log("swithced theme");
+	};
+
 	return (
 		<header id="navbar">
 			<nav>
@@ -15,6 +22,9 @@ export default function NavBar() {
 					<img className="map-icon" src={mapDark} />
 					Map Search
 				</Link>
+				<a onClick={switchTheme}>
+					<img className="theme-icon" src={themeDark} />
+				</a>
 			</nav>
 		</header>
 	);
