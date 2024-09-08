a044e2ecf92ac7d29b5f3dda411d8858abe15cba
Jadon Hansen <jadonhansen@gmail.com>
2024-09-08 16:19:58 +0200

    eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,16 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
+	includeIgnoreFile(gitignorePath),
 	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
@@ -16,5 +24,6 @@ export default [
 			"react/react-in-jsx-scope": "off",
 			"@typescript-eslint/no-unused-vars": "warn",
 		},
+		// ignores: ["dist/"]
 	},
 ];
