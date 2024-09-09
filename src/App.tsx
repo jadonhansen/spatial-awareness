c68b5502bbbc0ca37fa61eb4a71f8f9e5c7e73f6
Jadon Hansen <jadonhansen@gmail.com>
2024-09-09 15:58:06 +0200

    map theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,22 @@ import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import NavBar from "./components/NavBar";
 import { Theme } from "./types/types";
+import { getTheme } from "./helpers/helpers";
 
 function App() {
 	useEffect(() => {
-		getTheme();
+		const theme = getTheme();
+		setTheme(theme);
 
+		// listens for device theme changes
 		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
 			setTheme(event.matches ? "dark" : "light");
 		});
 	}, []);
 
-	const getTheme = () => {
-		const theme = localStorage.getItem("theme");
-
-		if (theme) {
-			if (theme === "dark") setTheme("dark");
-			if (theme === "light") setTheme("light");
-			return;
-		} else {
-			const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
-			if (prefersDarkTheme.matches) {
-				setTheme("dark");
-				return;
-			}
-
-			const prefersLightTheme = window.matchMedia("(prefers-color-scheme: light)");
-			if (prefersLightTheme.matches) {
-				setTheme("light");
-				return;
-			}
-		}
-
-		setTheme("dark");
-	};
-
 	const setTheme = (theme: Theme) => {
 		document.documentElement.setAttribute("theme", theme);
+		localStorage.setItem("theme", theme);
 	};
 
 	return (
